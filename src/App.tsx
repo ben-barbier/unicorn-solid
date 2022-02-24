4098f89aff429afca7a1a264ce4eca06a7c33571
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2022-02-24 23:06:29 +0100

    :sparkles: display unicorns

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
 
+interface Unicorn {
+    id: number,
+    name: string,
+    birthyear: number,
+    weight: number,
+    photo: string,
+    hobbies: string[],
+    capacities: number[]
+}
+
+const unicorns: Unicorn[] = await fetch('https://raw.githubusercontent.com/ben-barbier/unicorn-back-data/master/unicorns.json').then(res => res.json());
+
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class={styles.App}>
+            <header class={styles.header}>
+                { unicorns.map(unicorn =>
+                    <div>
+                        {unicorn.id}
+                        {unicorn.name}
+                        {unicorn.birthyear}
+                        {unicorn.weight}
+                        {unicorn.photo}
+                        {unicorn.hobbies}
+                        {unicorn.capacities}
+                    </div>
+                ) }
+            </header>
+        </div>
+    );
 };
 
 export default App;
